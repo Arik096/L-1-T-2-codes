2e7c2f655eb5a4e3eaf8af31146c6b0e878b48ed
Arik096 <arikbncc@gmail.com>
2017-04-17 22:19:57 +0600

    Add files via upload

---
+++ 1006 light oj.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <cstring>
+#define maxn 10003
+using namespace std;
+long long fn[maxn];
+int main() {
+int n, caseno = 0, cases,i;
+scanf("%d", &cases);
+while( cases-- )
+{
+fn[6]=0;
+for (i=0;i<6;i++)
+{
+scanf("%lld",&fn[i]); fn[i] = fn[i] % 10000007;
+fn[6]=(fn[6]+fn[i]) % 10000007;
+}
+scanf("%d",&n);
+for (i=7;i<=n;i++) fn[i]=(fn[i-1]+fn[i-2]+fn[i-3]+fn[i-4]+fn[i-5]+fn[i-6])% 10000007;
+printf("Case %d: %lld\n", ++caseno, fn[n] );
+}
+return 0;
+}
---
+++ Untitled1.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main () {
+int i,cases;
+long int n,d,temp;
+FILE *fin;
+if( (fin=fopen(11ENWprobb,"r"))==NULL ) exit(1);
+fscanf(fin,"%d",&cases);
+for(i=0;i<cases;i++) {
+fscanf(fin,"%ld %*c %ld",&n,&d);
+while(n!=1 || d!=1) {
+if( (n-d)>0 ) {
+temp=n-d;
+n=d;
+d=temp;
+printf("R");
+}
+else {
+temp=d-n;
+d=n;
+n=temp;
+printf("L");
+}
+}
+printf("\n");
+}
+fclose(fin);
+exit(0);
+}
